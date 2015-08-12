8efbc8fd702fd4e840035ac7463fe66d1a057d33
evahunger <e.hunger@zeppelin-university.net>
2015-08-12 10:14:58 +0200

    modified db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810154902) do
+ActiveRecord::Schema.define(version: 20150810123502) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20150810154902) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
   end
 
 end
